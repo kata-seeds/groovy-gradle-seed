2f7877e7b58d01404a1c5c8252cd6cac2aed588e
Sam Gleske <sam.mxracer@gmail.com>
2015-02-19 20:57:33 -0500

    Groovy seed

---
+++ src/main/groovy/kata/seeds/example/Person.groovy
@@ -0,0 +1,24 @@
+package kata.seeds.example
+
+/**
+  A nice Person who likes to greet you.
+ */
+class Person {
+    /**
+      Stores the greeting a person gives.
+     */
+    String greeting
+    /**
+      Instantate a person with a default greeting.  Default greeting is "Hello!".
+     */
+    def Person() {
+        greeting = "Hello!"
+    }
+    /**
+      Will output a greeting from the Person.
+      @return A simple greeting.
+     */
+    public String greet() {
+        greeting
+    }
+}
---
+++ src/main/groovy/kata/seeds/example/package-info.groovy
@@ -0,0 +1,4 @@
+/**
+    This package provides a working example of a groovy project with groovydocs.
+ */
+package kata.seeds.example
